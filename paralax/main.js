8518895c2791270e8a248a9431a601cafc94c825
KNIGHTISH <nitishmeswal@gmail.com>
2026-05-14 01:22:18 +0530

    Initial commit: Add library site collection with multiple professional-grade website templates

---
+++ paralax/main.js
@@ -0,0 +1,81 @@
+const parallax_el = document.querySelectorAll(".parallax");
+const main = document.querySelector("#main");
+let xValue = 0,
+  yValue = 0;
+
+let rotateDegree = 0;
+
+function update(cursorPosition) {
+  parallax_el.forEach((el) => {
+    let speedX = el.dataset.speedx;
+    let speedY = el.dataset.speedy;
+    let speedZ = el.dataset.speedz;
+    let rotationSpeed = el.dataset.rotation;
+
+    let isInLeft =
+      parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+    let zValue =
+      (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
+    el.style.transform = ` perspective(2300px) translateZ(${
+      zValue * speedZ
+    }px) rotateY(${rotateDegree * rotationSpeed}deg) translateX(calc(-50% + ${
+      -xValue * speedX
+    }px)) translateY(calc(-50% + ${yValue * speedY}px))`;
+  });
+}
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+  if (timeline.isActive()) return;
+
+  xValue = e.clientX - window.innerWidth / 2;
+  yValue = e.clientY - window.innerHeight / 2;
+  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+  update(e.clientX);
+});
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+  .filter((el) => !el.classList.contains("text"))
+  .forEach((el) => {
+    timeline.from(
+      el,
+      {
+        top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+        duration: 3.5,
+        ease: "power3.out"
+      },
+      "1"
+    );
+  });
+timeline
+  .from(
+    ".text h1",
+    {
+      y:
+        window.innerHeight -
+        document.querySelector(".text h1").getBoundingClientRect().top +
+        200,
+      duration: 2
+    },
+    "2.5"
+  )
+  .from(
+    ".text h2",
+    {
+      y: -150,
+      opacity: 0,
+      duration: 1.5
+    },
+    "3"
+  )
+  .from(
+    ".hide",
+    {
+      opacity: 0,
+      duration: 1.5
+    },
+    "3"
+  );
